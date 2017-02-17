8a4082eed7257132aaa91a4cad2e8f41c815296a
Suzanne <suzanne.aitchison@gmail.com>
2017-02-17 11:01:38 +0000

    film class created and tested in console

--- console.rb
+++ console.rb
@@ -1,6 +1,7 @@
 require('pry')
 require('PG')
 require_relative('./models/customer.rb')
+require_relative('./models/film.rb')
 require_relative('./db/sql_runner.rb')
 
 
@@ -10,6 +11,11 @@ customer1 = Customer.new({
   })
 
 customer1.save
+
+film1 = Film.new({
+  'title' => 'Mighty Ducks',
+  'price' => 6.50
+  })
 
 binding.pry
 nil
